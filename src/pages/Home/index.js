3227e9bda7ca4c20a45d76a7756bdb7024b2341c
Diego Sano <diegocsano@outlook.com>
2023-02-28 00:20:03 -0300

    refactor: contact list content to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,91 @@
+import * as S from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <S.Container>
+      <S.Header>
+        <strong>3 contacts</strong>
+
+        <a href="/">New contact</a>
+      </S.Header>
+
+      <S.ListContainer>
+        <header>
+          <button type="button">
+            <span>Name</span>
+
+            <img src={arrow} alt="Arrow icon" />
+          </button>
+        </header>
+
+        <S.Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Diego Sano</strong>
+              <small>Instagram</small>
+            </div>
+
+            <span>vedrivop@dodog.mq</span>
+            <span>23bb8c6c-daf3-5813-9ad9-d69b9bb517ee</span>
+          </div>
+
+          <div>
+            <a href="/">
+              <img src={edit} alt="Edit icon" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Trash icon" />
+            </button>
+          </div>
+        </S.Card>
+        <S.Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Diego Sano</strong>
+              <small>Instagram</small>
+            </div>
+
+            <span>vedrivop@dodog.mq</span>
+            <span>23bb8c6c-daf3-5813-9ad9-d69b9bb517ee</span>
+          </div>
+
+          <div>
+            <a href="/">
+              <img src={edit} alt="Edit icon" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Trash icon" />
+            </button>
+          </div>
+        </S.Card>
+        <S.Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Diego Sano</strong>
+              <small>Instagram</small>
+            </div>
+
+            <span>vedrivop@dodog.mq</span>
+            <span>23bb8c6c-daf3-5813-9ad9-d69b9bb517ee</span>
+          </div>
+
+          <div>
+            <a href="/">
+              <img src={edit} alt="Edit icon" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Trash icon" />
+            </button>
+          </div>
+        </S.Card>
+      </S.ListContainer>
+    </S.Container>
+  );
 }
