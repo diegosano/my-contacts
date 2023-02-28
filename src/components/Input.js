e24fee0027612af9e6ad2df469160da7521fb455
Diego Sano <diegocsano@outlook.com>
2023-02-28 00:40:28 -0300

    feat: input and select styled components

---
+++ src/components/Input.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  width: 100%;
+  background: #ffffff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  border: none;
+  height: 52px;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+  border: 2px solid #ffffff;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,17 @@
+import { Input } from '../../components/Input';
 import { PageHeader } from '../../components/PageHeader';
+import { Select } from '../../components/Select';
 
 export function NewContact() {
-  return <PageHeader title="New Contact" />;
+  return (
+    <>
+      <PageHeader title="New Contact" />
+
+      <Input type="text" placeholder="Name" />
+
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
+  );
 }
