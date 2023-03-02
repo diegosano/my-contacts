7a6efd04e21e36919aa19f4370db722388120ea1
Diego Sano <diegocsano@outlook.com>
2023-03-02 13:30:16 -0300

    feat: toast message variants

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -7,7 +7,7 @@ import checkCircle from '../../../assets/images/icons/check-circle.svg';
 
 export function ToastMessage({ text, type }) {
   return (
-    <S.Container>
+    <S.Container variant={type}>
       {type === 'danger' && <img src={xCircle} alt="X icon" />}
 
       {type === 'success' && <img src={checkCircle} alt="Check icon" />}
