743c6c6a1c467bd63a8e9f5f2fd37e1cf9f758d9
Diego Sano <diegocsano@outlook.com>
2023-03-05 04:03:03 -0300

    refactor: home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,3 @@
-import {
-  useCallback, useEffect, useMemo, useState,
-} from 'react';
 import { Link } from 'react-router-dom';
 
 import * as S from './styles';
@@ -8,8 +5,7 @@ import { Loader } from '../../components/Loader';
 import { Modal } from '../../components/Modal';
 import { Button } from '../../components/Button';
 
-import ContactsService from '../../services/ContactsService';
-import { toast } from '../../utils/toast';
+import { useHome } from './useHome';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
@@ -17,85 +13,25 @@ import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
 import magnifierQuestion from '../../assets/images/magnifier-question.svg';
-import { useSafeAsyncState } from '../../hooks/useSafeAsyncState';
 
 export function Home() {
-  const [contacts, setContacts] = useSafeAsyncState([]);
-  const [orderBy, setOrderBy] = useState('ASC');
-  const [search, setSearch] = useState('');
-  const [isLoading, setIsLoading] = useSafeAsyncState(true);
-  const [hasError, setHasError] = useSafeAsyncState(false);
-  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
-  const [contactBeingDeleted, setContactBeingDeleted] = useState({});
-  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
-
-  const loadContacts = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const contactsList = await ContactsService.findAll(orderBy);
-      setContacts(contactsList);
-      setHasError(false);
-    } catch {
-      setHasError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [orderBy, setContacts, setHasError, setIsLoading]);
-
-  useEffect(() => {
-    loadContacts();
-  }, [loadContacts]);
-
-  function handleToggleOrderBy() {
-    setOrderBy((prevState) => (prevState === 'ASC' ? 'DESC' : 'ASC'));
-  }
-
-  function handleChangeSearch(event) {
-    setSearch(event.target.value);
-  }
-
-  const handleTryAgain = useCallback(async () => {
-    loadContacts();
-  }, [loadContacts]);
-
-  function handleDeleteContact(contact) {
-    setIsDeleteModalVisible(true);
-    setContactBeingDeleted(contact);
-  }
-
-  const handleCloseDeleteModal = useCallback(() => {
-    setIsDeleteModalVisible(false);
-    setContactBeingDeleted({});
-  }, []);
-
-  const handleConfirmDeleteContact = useCallback(async () => {
-    try {
-      setIsLoadingDelete(true);
-      await ContactsService.delete(contactBeingDeleted.id);
-
-      setContacts(
-        (prevState) => prevState.filter((contact) => contact.id !== contactBeingDeleted.id),
-      );
-      handleCloseDeleteModal();
-
-      toast({
-        type: 'success',
-        text: 'Contact successfully deleted',
-      });
-    } catch {
-      toast({
-        type: 'danger',
-        text: 'An error occurred while deleting the contact!',
-      });
-    } finally {
-      setIsLoadingDelete(false);
-    }
-  }, [contactBeingDeleted.id, handleCloseDeleteModal]);
-
-  const filteredContacts = useMemo(
-    () => contacts.filter((contact) => contact.name.toLowerCase().includes(search.toLowerCase())),
-    [contacts, search],
-  );
+  const {
+    isLoading,
+    contactBeingDeleted,
+    handleCloseDeleteModal,
+    handleConfirmDeleteContact,
+    isDeleteModalVisible,
+    isLoadingDelete,
+    contacts,
+    search,
+    handleChangeSearch,
+    hasError,
+    filteredContacts,
+    handleTryAgain,
+    orderBy,
+    handleToggleOrderBy,
+    handleDeleteContact,
+  } = useHome();
 
   return (
     <S.Container>
