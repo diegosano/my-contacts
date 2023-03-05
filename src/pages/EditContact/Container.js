7ecf61eed1a179211f698505a256a18fba848fcc
Diego Sano <diegocsano@outlook.com>
2023-03-05 03:47:39 -0300

    refactor: edit contact to presentation container patter

---
+++ src/pages/EditContact/Container.js
@@ -0,0 +1,74 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import { Presentation } from './Presentation';
+
+import ContactsService from '../../services/ContactsService';
+import { toast } from '../../utils/toast';
+import { useSafeAsyncState } from '../../hooks/useSafeAsyncState';
+import { useSafeAsyncAction } from '../../hooks/useSafeAsyncAction';
+
+export function Container() {
+  const [isLoading, setIsLoading] = useSafeAsyncState(true);
+  const [contactName, setContactName] = useSafeAsyncState('');
+  const contactFormRef = useRef(null);
+  const { id } = useParams();
+  const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        setIsLoading(true);
+        const contact = await ContactsService.findOne(id);
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setContactName(contact.name);
+          setIsLoading(false);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          toast({
+            type: 'danger',
+            text: 'Contact not found',
+          });
+          history.push('/');
+        });
+      }
+    }
+
+    if (contactFormRef.current) {
+      loadContact();
+    }
+  }, [history, id, setContactName, setIsLoading, safeAsyncAction]);
+
+  const handleSubmit = useCallback(
+    async (contact) => {
+      try {
+        const contactData = await ContactsService.update(id, contact);
+
+        setContactName(contactData.name);
+        toast({
+          type: 'success',
+          text: 'Contact successfully updated',
+        });
+      } catch {
+        toast({
+          type: 'danger',
+          text: 'An error occurred while updating the contact!',
+        });
+      }
+    },
+    [id, setContactName],
+  );
+
+  return (
+    <Presentation
+      isLoading={isLoading}
+      contactName={contactName}
+      contactFormRef={contactFormRef}
+      onSubmit={handleSubmit}
+    />
+  );
+}
--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,81 +1,14 @@
-import { useCallback, useEffect, useRef } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-
-import { ContactForm } from '../../components/ContactForm';
-import { PageHeader } from '../../components/PageHeader';
-import { Loader } from '../../components/Loader';
-
-import ContactsService from '../../services/ContactsService';
-import { toast } from '../../utils/toast';
-import { useSafeAsyncState } from '../../hooks/useSafeAsyncState';
-import { useSafeAsyncAction } from '../../hooks/useSafeAsyncAction';
+import { Container } from './Container';
+// import { Presentation } from './Presentation';
+// or
+// export { default as Presentation } from './Presentation';
 
 export function EditContact() {
-  const [isLoading, setIsLoading] = useSafeAsyncState(true);
-  const [contactName, setContactName] = useSafeAsyncState('');
-  const contactFormRef = useRef(null);
-  const { id } = useParams();
-  const history = useHistory();
-  const safeAsyncAction = useSafeAsyncAction();
-
-  useEffect(() => {
-    async function loadContact() {
-      try {
-        setIsLoading(true);
-        const contact = await ContactsService.findOne(id);
-
-        safeAsyncAction(() => {
-          contactFormRef.current.setFieldsValues(contact);
-          setContactName(contact.name);
-          setIsLoading(false);
-        });
-      } catch {
-        safeAsyncAction(() => {
-          toast({
-            type: 'danger',
-            text: 'Contact not found',
-          });
-          history.push('/');
-        });
-      }
-    }
-
-    if (contactFormRef.current) {
-      loadContact();
-    }
-  }, [history, id, setContactName, setIsLoading, safeAsyncAction]);
-
-  const handleSubmit = useCallback(
-    async (contact) => {
-      try {
-        const contactData = await ContactsService.update(id, contact);
-
-        setContactName(contactData.name);
-        toast({
-          type: 'success',
-          text: 'Contact successfully updated',
-        });
-      } catch {
-        toast({
-          type: 'danger',
-          text: 'An error occurred while updating the contact!',
-        });
-      }
-    },
-    [id, setContactName],
-  );
-
-  return (
-    <>
-      <Loader isLoading={isLoading} />
-
-      <PageHeader title={isLoading ? 'Loading...' : `Edit ${contactName}`} />
-
-      <ContactForm
-        ref={contactFormRef}
-        buttonLabel="Save"
-        onSubmit={handleSubmit}
-      />
-    </>
-  );
+  return <Container />;
 }
+
+// another way is export as object
+// export {
+//   Container,
+//   Presentation,
+// };
